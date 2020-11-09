7d1236d470db2364a42d9223969d6d1b058cb0ea
lfeldman <lukasz.feldman@oracle.com>
2020-11-09 14:12:14 +0100

    Support for ORM Deploy button

--- oci-arch-se-vm-db-mig/compute.tf
+++ oci-arch-se-vm-db-mig/compute.tf
@@ -16,23 +16,22 @@ locals {
 
 resource "oci_core_instance" "compute_instance1" {
   availability_domain = local.ad
-
   compartment_id      = var.compartment_ocid
   display_name        = "App-Server"
   shape               = var.instance_shape
-  subnet_id           = oci_core_subnet.subnet_1.id
   fault_domain        = "FAULT-DOMAIN-1"
-
-source_details {
+  source_details {
     source_type             = "image"
     source_id               = data.oci_core_images.InstanceImageOCID.images[0].id
     boot_volume_size_in_gbs = "50"
   }
-
-  metadata = {
-    ssh_authorized_keys = chomp(file(var.ssh_public_key))
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet_1.id
+    assign_public_ip = false 
   }
-
+  metadata = {
+    ssh_authorized_keys = tls_private_key.public_private_key_pair.public_key_openssh
+  }
   timeouts {
     create = "60m"
   }
